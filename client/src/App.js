824f054739ee601e2a3b1520a22859a3f4ca0907
devendra dhare <devendradhare03@gmail.com>
2023-08-20 22:02:21 +0530

    sended .git file to the perent folder

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, { useRef, useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import About from "./components/About";
+import Home from "./components/Home";
+import LiveMap from "./components/LiveMap";
+
+// for socket connection
+import socketIO from "socket.io-client";
+const socket = socketIO.connect("http://localhost:4000");
+// const socket = socketIO.connect("http://127.0.0.1:4000");
+// const socket = socketIO.connect("https://e0d9-2409-4043-4c1d-30e9-d108-c3a4-49d2-8393.ngrok-free.app");
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<h3>page is in production </h3>} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/live_map" element={<LiveMap web_socket={socket}/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
