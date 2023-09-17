371532475a1b0054b70bce2de35813ab38a3a73b
devendra dhare <devendradhare03@gmail.com>
2023-09-17 21:19:29 +0530

    added home page

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Fab from "@mui/material/Fab";
+import NavigationIcon from "@mui/icons-material/Navigation";
+import "./home.css";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>Collage Bus Live</h1>
+      <p>Track your collage bus live</p>
+      <Link to="/about">
+        <Fab variant="extended" size="medium" color="primary">
+          <NavigationIcon sx={{ mr: 1 }} />
+          Go to map
+        </Fab>
+      </Link>
+    </div>
+  );
+}
