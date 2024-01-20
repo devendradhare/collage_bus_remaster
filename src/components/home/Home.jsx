d3c0ce00378a0976b631ba4cd53460decfd4540e
devendra dhare <devendradhare03@gmail.com>
2024-01-20 14:45:43 +0530

    app discription improved

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="home">
       <h1>Collage Bus Live</h1>
-      <p>Track your collage bus live</p>
+      <p>Introducing our College Cruiser Tracker – the ultimate sidekick for a stress-free bus journey! Imagine having the power to know exactly where your college bus is in real-time. Our web app makes it a breeze for students and bus drivers to stay in the loop, ensuring a smooth and safe ride. It's like having a personal GPS for your daily adventures! 🚍</p>
       <Link to="/live_map">
         <Fab variant="extended" size="medium" color="primary">
           <NavigationIcon sx={{ mr: 1 }} />
